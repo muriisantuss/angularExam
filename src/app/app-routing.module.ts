7a7df8984f73e00bcf06023a19edf9538c66d10d
muriisantuss <muriisantuss@gmail.com>
2024-06-02 23:08:24 -0300

    (feat): create footer navbar and router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AuthorComponent } from './author/author.component';
+import { BookComponent } from './book/book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'author', component: AuthorComponent},
+  {path: 'book', component: BookComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
